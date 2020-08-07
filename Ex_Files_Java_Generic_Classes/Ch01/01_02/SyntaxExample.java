b02ee5bc7e27b54a86532c315da10d657d2ae859
ashwin.karki <ashwin.karki123@yahoo.com>
2020-08-07 15:31:04 +0545

    added

---
+++ Ex_Files_Java_Generic_Classes/Ch01/01_02/SyntaxExample.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package syntaxexample;
+/**
+ *
+ * @author Producer
+ */
+public class SyntaxExample {
+    public static void main(String[] args)
+    {
+     
+    }
+    
+}
